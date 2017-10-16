b35ed76a01c4fc3dd3ec50dcfbaea0c8bb5fdbe7
Mark Melendy <mmelendy@umich.edu>
2017-10-15 21:36:00 -0400

    Added 2d array math, working on correctness

--- mouse_reader.py
+++ mouse_reader.py
@@ -1,19 +1,50 @@
 from collections import deque
-import pyautogui
+import pyautogui, sys
+
+if len(sys.argv) != 3:
+	mon_x_div = 1
+	mon_y_div = 1
+	print "2 inputs not detected, making 1 monitor area"
+else:
+	try: 
+		mon_x_div = int(sys.argv[1])
+		mon_y_div = int(sys.argv[2])
+	except ValueError:
+		mon_x_div = 1
+		mon_y_div = 1
+		print "non int detected, making 1 monitor area"
+
+monitor_split = [[0 for i in range(mon_x_div)] for j in range(mon_y_div)]
 
 reader = open("movements.txt",'r')
-#mouse_points = deque([],100)
 
 mon_size = pyautogui.size()
 monitor1 = 0
 monitor2 = 0
 
+points_dict = {}
+
 for line in reader:
-	x,y = [int(x) for x in line.split()]
-	if x > mon_size[0]:
+	x, y = [int(x) for x in line.split()]
+	if x < mon_size[0]:
 		monitor1 += 1
+		x_pos = int(x/mon_size[0] * mon_x_div)
+		y_pos = int(y/mon_size[1] * mon_y_div)
+		monitor_split[x_pos][y_pos] += 1
 	else:
 		monitor2 += 1
 
-print "Monitor 1 had: ", monitor1, "points"
-print "Monitor 2 had: ", monitor2, "points"
+print monitor_split
+
+if monitor1 > 0:
+	for x in range(mon_x_div):
+		y_values = ''
+		for y in range(mon_y_div):
+			y_per = monitor_split[y][x] / monitor1
+			if y_per == 1:
+				y_per = 100
+			y_per = str(y_per) + '%'
+	   		y_values += y_per.rjust(4)
+	   	print y_values
+else:
+	print "movements.txt has no points in it"
--- mouse_tracker.py
+++ mouse_tracker.py
@@ -1,20 +1,21 @@
 #! python
-import pyautogui, sys, time
+import pyautogui, sys, time, os
 
 print('Press Ctrl-C to quit.')
 movement_file = open("movements.txt", 'w')
 
 try:
     while True:
-    	time.sleep(.01)
+    	time.sleep(.05)
         x, y = pyautogui.position()
         posStr = str(x) + ' ' + str(y)
         positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
         movement_file.write(posStr)
         movement_file.write('\n')
+        movement_file.flush()
         print positionStr,
         print '\b' * (len(positionStr) + 2),
         sys.stdout.flush()
 except KeyboardInterrupt:
-    movement_file.close()
     print('\n\nKeyboard exception received. Exiting.')
+    movement_file.close() 
